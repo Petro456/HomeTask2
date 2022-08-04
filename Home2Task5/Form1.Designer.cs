5271e26c47ccaad30df17fc23353401104165c2f
Petro456 <101942346+Petro456@users.noreply.github.com>
2022-08-04 11:16:04 +0300

    Добавьте файлы проекта.

---
+++ Home2Task5/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Home2Task5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 10;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(115, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "sbyte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(341, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "byte";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(585, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 87);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "short";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(341, 344);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 94);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ulong";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(115, 184);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 96);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ushort";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(585, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(137, 96);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "long";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(115, 344);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(132, 94);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "int";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(341, 184);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(134, 96);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "uint";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Home2Task5/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Home2Task5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show(this, "Від -128 до 127");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від 0 до 255");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від -32 768 до 32 767");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від 0 до 65 535");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від -2 147 483 648 до 2 147 483 647");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від 0 до 4 294 967 295");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від -9 223 372 036 854 775 808 до 9 223 372 036 854 775 807");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Від 0 до 18 446 744 073 709 551 615");
+        }
+    }
+}
